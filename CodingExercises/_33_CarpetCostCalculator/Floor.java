78810009568df37c0c050a965a3c36adc54f9cd9
Joshua Ho Gwok Hin <81602544+Joshua-Ho-Gwok-Hin@users.noreply.github.com>
2022-06-15 12:06:44 +0800

    Add files via upload

---
+++ CodingExercises/_33_CarpetCostCalculator/Floor.java
@@ -0,0 +1,27 @@
+package _33_CarpetCostCalculator;
+
+public class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double length){
+
+        if (width<0){
+            this.width = 0d;
+        }else {
+            this.width = width;
+        }
+
+        if (length<0){
+            this.length = 0d;
+        }else {
+            this.length = length;
+        }
+    }
+
+    public double getArea(){
+        return width * length;
+    }
+
+
+}
